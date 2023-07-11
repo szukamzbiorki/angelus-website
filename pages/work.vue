22f46bd870ebf3c6c160ccaf5355096262a53763
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2023-07-11 11:58:32 +0200

    siema

--- pages/work.vue
+++ pages/work.vue
@@ -16,24 +16,55 @@ definePageMeta({
         css: false,
         onBeforeEnter: (el) => {
             const { $gsap } = useNuxtApp()
-            $gsap.set(el, {
-                opacity: 0,
-                filter: "blur(7px)"
-            })
+            let mm = $gsap.matchMedia();
+
+            mm.add("(min-width: 820px)", () => {
+                $gsap.set(el, {
+                    opacity: 0,
+                    filter: "blur(7px)"
+                })
+            });
+
+            mm.add("(max-width: 819px)", () => {
+                const { $gsap } = useNuxtApp()
+                $gsap.set(el, {
+                    opacity: 0,
+                })
+            });
+
         },
         onEnter: (el, done) => {
             const { $gsap } = useNuxtApp()
-            $gsap.to(el, {
-                opacity: 1,
-                filter: "blur(0px)",
-                duration: .3
-            })
+            let mm = $gsap.matchMedia();
+            mm.add("(min-width: 820px)", () => {
+                $gsap.to(el, {
+                    opacity: 1,
+                    filter: "blur(0px)",
+                    duration: .3
+                })
+            });
+            mm.add("(max-width: 819px)", () => {
+                $gsap.to(el, {
+                    opacity: 1,
+                    duration: .3
+                })
+            });
         },
         onLeave: (el) => {
             const { $gsap } = useNuxtApp()
-            $gsap.to(el, {
-                opacity: 0,
-            })
+            let mm = $gsap.matchMedia();
+            mm.add("(min-width: 820px)", () => {
+                $gsap.to(el, {
+                    opacity: 0,
+                    filter: "blur(7px)"
+                })
+            });
+
+            mm.add("(max-width: 819px)", () => {
+                $gsap.to(el, {
+                    opacity: 0
+                })
+            });
         }
     }
 });
