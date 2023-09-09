1ddc51c2f725eb24a167297463826f6e90aa1900
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2023-09-09 17:47:41 +0200

    yo

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,22 @@
 .content {
     margin-left: 250px;
     margin-top: 1ch;
+    max-height: calc(100vh - 43px);
+    overflow: scroll;
+}
+
+.content::-webkit-scrollbar {
+    width: 5px;
+}
+
+/* Track */
+.content::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+
+/* Handle */
+.content::-webkit-scrollbar-thumb {
+    background: #000000;
 }
 
 @media screen and (max-width: 820px) {
